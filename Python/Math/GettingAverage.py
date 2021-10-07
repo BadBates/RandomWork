8bbbdcc88fe82a3b51a849c5d207120953894520
Ryan Bates <rybates215@gmail.com>
2021-10-07 15:47:42 -0600

    A couple warm ups
    Have a name and number ordering system as well as a way to have a person put in three numbers to find the mean of them

---
+++ Python/Math/GettingAverage.py
@@ -0,0 +1,10 @@
+a = int(input("WhatNumber"))
+print("")
+b = int(input("WhatNumber"))
+print("")
+c = int(input("WhatNumber"))
+print("")
+
+def Average(int, int, int):
+    return avr 
+print (Average(a, b, c))
---
+++ Python/Ordering/NameOrdering.py
@@ -0,0 +1,6 @@
+file = "Names.txt"
+inFile = open(file, 'r')
+print("the names in alphabetical order: ")
+for i in sorted (inFile):
+    if(inFile):
+        print(i)
