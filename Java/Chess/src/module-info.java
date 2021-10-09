0818c7d200998a49dc31b030c73190bf53ddfdaf
Ryan Bates <rybates215@gmail.com>
2021-10-08 23:39:40 -0600

    Time calculator
    Finds out how many seconds up to months you were born by the users input.
    Also have the foundation for making a chess board in the next project.

---
+++ Java/Chess/src/module-info.java
@@ -0,0 +1,12 @@
+
+import java.Awt.Color;
+import java.Awt.Graphics;
+import java.Awt.image.BufferedImage;
+import java.util.LinkedList;
+import javax.Swing.JFrame;
+
+module Chess {
+	public static void main(String[] args) { // this will be the game board interacting with all the pieces
+		
+	}
+}
---
+++ Python/Math/Time.py
@@ -0,0 +1,28 @@
+years = 31536000   #real answer 3.154e+7
+TimePassed = int(input("How Old Are you exact age: ")) * years #Time Passed
+
+
+print("You are :", TimePassed, " seconds old.")
+
+#Time passing in seconds.
+months = 2628288
+weeks = 604800
+days = 86400
+hours = 3600
+minutes = 60
+seconds = 1
+
+Mold = TimePassed/ months
+print("You are :",Mold, " months old.")
+
+Wold = TimePassed / weeks
+print("You are :",Wold, " weeks old.")
+    
+Dold = TimePassed / days
+print("You are :",Dold, " days old.")
+
+Hold = TimePassed / hours
+print("You are :",Hold, " hours old.")
+    
+MIold = TimePassed / minutes
+print("You are :",Hold, " minutes old.")
