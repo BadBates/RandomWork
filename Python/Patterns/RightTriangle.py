dfe11314cdf04482217218148231f6d86d75716c
Ryan Bates <rybates215@gmail.com>
2021-10-14 16:51:22 -0600

    inverting a triangle

---
+++ Python/Patterns/RightTriangle.py
@@ -0,0 +1,13 @@
+import argparse
+#n = int(input("how many rows for the triangle?:"))
+
+def right_triangle(n):
+    for row in range(1, n+1):
+        start_index = (n-row)
+        print(" " * start_index, end = "")
+        for star in range(row):
+            print("*", end= "")
+        
+        print("")
+
+right_triangle(10)
