49066f9eab6b81196161e0dad8e15ef0068d71b8
Ryan Bates <rybates215@gmail.com>
2021-11-12 13:48:09 -0700

    Linked
    made a window with the linked SFML project.

--- C++/Tetris/Tetris/Main.cpp
+++ C++/Tetris/Tetris/Main.cpp
@@ -1,1 +1,31 @@
 #pragma once
+#include <iostream>
+#include <Graphics.hpp>
+#include <time.h>
+#include <Window.hpp>
+using namespace std;
+using namespace sf;
+
+namespace Ryan {
+
+
+	int main() {
+		RenderWindow window(VideoMode(320, 480), "Start the Game....");
+
+		while (window.isOpen()) {
+			Event e;
+			while (window.pollEvent(e)) {
+				if (e.type == Event::Closed)
+					window.close();
+
+			}
+
+			window.clear(Color::Black);
+			window.display();
+		}
+
+
+		system("puase");
+		return 0;
+	}
+}
